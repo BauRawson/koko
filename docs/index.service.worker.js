0a1677e0e9e92116218c26213bd7c6c5323b5e8c
BauRawson <bautistarawson@gmail.com>
2025-06-09 13:22:08 -0300

    - Update build

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1749484635|10232298101';
+const CACHE_VERSION = '1749486081|11677659653';
 /** @type {string} */
 const CACHE_PREFIX = 'Koko&Friends-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
